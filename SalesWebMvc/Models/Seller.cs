32dfcada4aa906f39c8347a30bb063ec790325c3
Marcio silva <marcioadsworks@gmail.com>
2019-01-14 21:27:56 -0200

    validation

--- SalesWebMvc/Models/Seller.cs
+++ SalesWebMvc/Models/Seller.cs
@@ -9,16 +9,24 @@ namespace SalesWebMvc.Models
     public class Seller
     {
         public int Id { get; set; }
-        public string Name { get; set; }
 
+        [Required(ErrorMessage ="{0} required")]
+        [StringLength(60,MinimumLength =3,ErrorMessage ="{0} size shoud be between 3 and 60")]
+        public string Name { get; set; }
+        
+        [Required(ErrorMessage = "{0} required")]
+        [EmailAddress(ErrorMessage ="Enter a valid Email") ]
         [DataType(DataType.EmailAddress)]
         public string Email { get; set; }
-
+        
+        [Required(ErrorMessage = "{0} required")]
         [Display(Name ="Birth Date")]
         [DataType(DataType.Date)]
         [DisplayFormat(DataFormatString ="{0:dd/MM/yyy}")]
         public DateTime BirthDate { get; set; }
-
+        
+        [Required(ErrorMessage = "{0} required")]
+        [Range(100.0,50000.0,ErrorMessage ="{0} must be from {1} to {2}")]
         [Display(Name ="Base Salary")]
         [DisplayFormat(DataFormatString ="{0:F2}")]
         public double BaseSalary { get; set; }
